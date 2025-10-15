01e2a6f471a988ccc71bdefb7706b88cbb855409
YamatoDev <bibaocomatthew@gmail.com>
2025-10-15 17:34:17 +0800

    Added icons

--- bars.php
+++ bars.php
@@ -3,22 +3,49 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand fw-bold pe-lg-5" href="#">KKB</a>
+    <a class="navbar-brand fw-bold pe-lg-5" href="#">Apprise</a>
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false" data-bs-scroll="true">
       <div class="offcanvas-header">
-        <a class="offcanvas-title px-2 rounded-3 text-decoration-none" id="offcanvasNavbarLabel" href="#">KKB</a>
+        <a class="offcanvas-title px-2 rounded-3 text-decoration-none" id="offcanvasNavbarLabel" href="#">Apprise</a>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body d-flex flex-column">
         <ul class="navbar-nav justify-content-start flex-grow-1">
           <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="#">Home</a>
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\Dashboard.png" alt="Dashboard" width="24" height="24">
+              <span>Dashboard</span>
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="#">Projects</a>
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\Assignment.png" alt="Projects" width="24" height="24">
+              <span>Projects</span>
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="#">Connect</a>
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\People.png" alt="People" width="24" height="24">
+              <span>People</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\Calendar.png" alt="Calendar" width="24" height="24">
+              <span>Calendar</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\Settings.png" alt="Settings" width="24" height="24">
+              <span>Settings</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">
+              <img src="Images\Icons\Logout.png" alt="Logout" width="24" height="24">
+              <span>Logout</span>
+            </a>
           </li>
         </ul>
       </div>
