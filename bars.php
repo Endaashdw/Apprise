98e4e2cdedb218b4e505fe69bb66907c8d80ca5b
learntheba6 <kaizen.yomyom@gmail.com>
2025-10-15 16:02:44 +0800

    Added Navbar and Sidebar

---
+++ bars.php
@@ -0,0 +1,27 @@
+<nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand fw-bold pe-lg-5" href="#">KKB</a>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false" data-bs-scroll="true">
+      <div class="offcanvas-header">
+        <a class="offcanvas-title px-2 rounded-3 text-decoration-none" id="offcanvasNavbarLabel" href="#">KKB</a>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body d-flex flex-column">
+        <ul class="navbar-nav justify-content-start flex-grow-1">
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-1 my-1 ps-2" href="#">Connect</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php include("main_header.php"); ?>
-
+<?php include("bars.php"); ?>
 
 
 <?php include("main_footer.php"); ?>
