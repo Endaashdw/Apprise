7d66943ad9f4637267480288e25f37dc02523976
YamatoDev <bibaocomatthew@gmail.com>
2025-11-03 23:40:20 +0800

    Updated bars

--- includes/bars.php
+++ includes/bars.php
@@ -1,46 +1,59 @@
+<?php
+include("Includes/mysql_connect.php");
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+// get all projects that the user is a member of
+$project_query = "
+    SELECT p.project_id, p.project_name 
+    FROM projects p
+    JOIN team_users tu ON p.team_id = tu.team_id
+    WHERE tu.user_id = '$user_id'
+";
+$project_result = mysqli_query($conn, $project_query);
+?>
+
 <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand fw-bold pe-lg-5" href="#">Apprise</a>
+
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false" data-bs-scroll="true">
       <div class="offcanvas-header">
         <a class="offcanvas-title px-2 rounded-3 text-decoration-none" id="offcanvasNavbarLabel" href="#">Apprise</a>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
+
       <div class="offcanvas-body d-flex flex-column">
         <ul class="navbar-nav justify-content-start flex-grow-1">
+          <!-- Static Pages -->
           <li class="nav-item">
             <a class="nav-link mx-1 my-1 ps-2" href="home.php">
               <img src="Assets/Images/Icons/Dashboard.png" alt="Dashboard" width="24" height="24">
               <span>Dashboard</span>
             </a>
           </li>
+
+          <!-- Dynamic Project Links -->
+          <?php while ($project = mysqli_fetch_assoc($project_result)) : ?>
           <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="projects.php">
-              <img src="Assets/Images/Icons/Assignment.png" alt="Projects" width="24" height="24">
-              <span>Projects</span>
+            <a class="nav-link mx-1 my-1 ps-2" href="projects.php?project_id=<?php echo $project['project_id']; ?>">
+              <img src="Assets/Images/Icons/Assignment.png" alt="Project" width="24" height="24">
+              <span><?php echo htmlspecialchars($project['project_name']); ?></span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="team.php">
-              <img src="Assets/Images/Icons/People.png" alt="People" width="24" height="24">
-              <span>People</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-1 my-1 ps-2" href="calendar.php">
-              <img src="Assets/Images/Icons/Calendar.png" alt="Calendar" width="24" height="24">
-              <span>Calendar</span>
-            </a>
-          </li>
+          <?php endwhile; ?>
+
           <li class="nav-item">
             <a class="nav-link mx-1 my-1 ps-2" href="#">
               <img src="Assets/Images/Icons/Settings.png" alt="Settings" width="24" height="24">
               <span>Settings</span>
             </a>
           </li>
+
           <li class="nav-item">
             <a class="nav-link mx-1 my-1 ps-2" href="Actions/login/logout.php">
               <img src="Assets/Images/Icons/Logout.png" alt="Logout" width="24" height="24">
